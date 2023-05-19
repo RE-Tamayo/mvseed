e4a7063242eccfd6baec08e9caead73406d29666
RE-Tamayo <rosas.emerjoe.tamayo@gmail.com>
2023-05-19 09:42:53 +0800

    Add framework files

---
+++ src/models/SampleModel.php
@@ -0,0 +1,22 @@
+<?php
+namespace App\models;
+
+use Mvseed\Application\BaseModel;
+
+class SampleModel extends BaseModel
+{
+    public function test_orm()
+    {
+        // $this->addTable('test', 'id', ['a', 'b', 'c']);
+        // $this->addTable('test3', 'id', ['x']);
+        // $test = $this->useTable('test');
+        // $test3 = $this->useTable('test3');
+        // $test->perform_create(['a' => 'aaaq', 'c' => 'cccq', 'b' => 'bbbq']);
+        // $test->perform_update(['a' => 'aaa', 'c' => 'ccc', 'b' => 'bbb'], 'id', '6');
+        // $test->perform_delete('id', '11');
+        // $data = $test->perform_fetch(['a', 'c', 'b'], 'id', '6');
+        // $data = $test->perform_fetch_all(['a', 'c', 'b']);
+        // echo json_encode($data);
+        // require 'src/cache/cache_test.php';
+    }
+}
